395f26005db3c4fc3b17c6b18964933825dda5b9
yugo-ibuki <y.ibuki91@gmail.com>
2023-12-24 13:29:27 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import "swiper/css";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+const data: string[] = ["Slide 1", "Slide 2", "Slide 3", "Slide 4"];
+
+function App() {
+  return (
+    <>
+      <Swiper
+        spaceBetween={50}
+        slidesPerView={3}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => console.log(swiper)}
+      >
+        {data.map((d) => (
+          <SwiperSlide>
+            <div style={{ background: "grey" }}>{d}</div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
+}
+
+export default App;
