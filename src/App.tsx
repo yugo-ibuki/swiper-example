0104373c2e5e23f054d990b3d262be97c092e8af
yugo-ibuki <y.ibuki91@gmail.com>
2023-12-24 17:48:01 +0900

    add pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "swiper/css";
+import { Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 const data: string[] = ["Slide 1", "Slide 2", "Slide 3", "Slide 4"];
@@ -11,6 +12,10 @@ function App() {
         slidesPerView={3}
         onSlideChange={() => console.log("slide change")}
         onSwiper={(swiper) => console.log(swiper)}
+        pagination={{
+          type: "fraction",
+        }}
+        modules={[Pagination]}
       >
         {data.map((d) => (
           <SwiperSlide>
